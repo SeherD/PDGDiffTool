7cfc4e4f6f96f87d8ffa8708a3d13049418a16fa
lucasjohannson <lucas.johannson1@ucalgary.ca>
2022-03-29 15:34:42 -0600

    added line parse file currently just prints content TODO: make the output available to other classes

---
+++ helper/lineParse.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*; //just making sure
+
+
+public class lineParse {
+	public static void main(String[] args) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(
+					"/Users/lucasjohannson/lighthouse/SENG541/PDGDiffTool/input/ifTest.java"));
+			String line = reader.readLine();
+
+      //create dictionary to store line data 
+      Hashtable<Integer, String> lineContent = new Hashtable<Integer, String>();
+      //init lineNo variable to create keys
+      Integer lineNo = 0;
+      //loop while file not done
+			while (line != null) {
+        //remove white space for dictionary values 
+        String line1 = line.replaceAll("\\s+","");
+        //add line with no white space to the dict with key being lineNo
+        lineContent.put(lineNo, line1);
+        //print line data for review
+				System.out.println(line);
+				// increase the line number
+        lineNo ++; 
+        // read next line
+				line = reader.readLine();
+			}
+      //close reader
+			reader.close();
+      //todo export data for use in other files
+      System.out.println("content of dict: "+ lineContent);
+      //catch io errors 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
